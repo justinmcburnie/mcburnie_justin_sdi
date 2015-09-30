5d23e0daedb6fe6283156c8905dbe2cdcee06a72
justinmcburnie <jmcburnie@fullsail.edu>
2015-09-30 15:52:10 -0400

    Add Console.logs
    added console logs

--- Projects/Mcburnie_justin_output_assignment/script.js
+++ Projects/Mcburnie_justin_output_assignment/script.js
@@ -6,11 +6,12 @@
 // About me Varibles
 var myFirstName     ="Justin";
 var myLastName      ="McBurnie";
-var myDegreeProgram ="Webdesign and development";
-var myKids          ="2 kids";
+var myDegreeProgram ="Webdesign and Development program";
+var myKids          ="2";
 var myWork          ="Amazon.com";
 
 //outputs
 
-console.log(myFirstName);
-console.log
+console.log(Hello my name is myFirstName + myLastName);
+console.log(I have +myKids kids who are 4 and 2. )
+console.log(I am enrolled in the + myDegreeProgram)
