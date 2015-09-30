a4d11ba9c866b03b481b29cb3a489a1a4bfcaca8
justinmcburnie <jmcburnie@fullsail.edu>
2015-09-30 15:41:19 -0400

    Add script src
    added script source

---
+++ Projects/Mcburnie_justin_output_assignment/script.js
@@ -0,0 +1,15 @@
+/**Justin McBurnie
+ * Term 1510
+ * Output assignment
+ */
+
+// About me Varibles
+var myFirstName     ="Justin";
+var myLastName      ="McBurnie";
+var myDegreeProgram ="Webdesign and development";
+var myKids          ="2 kids";
+var myWork          ="Amazon.com";
+
+//outputs
+
+console.log(myFirstName);
