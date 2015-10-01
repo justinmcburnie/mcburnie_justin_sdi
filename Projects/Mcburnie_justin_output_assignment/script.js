636bc2eed36ba9e63023fca4b02621fbf21f861f
justinmcburnie <jmcburnie@fullsail.edu>
2015-10-01 13:06:07 -0400

    FINALLYYY
    Finally got my code working

---
+++ Projects/Mcburnie_justin_output_assignment/script.js
@@ -0,0 +1,18 @@
+/**Justin McBurnie
+ * Term 1510
+ * Output assignment
+ */
+
+// About me Varibles
+var myFirstName     = "Justin";
+var myLastName      = "McBurnie";
+var myDegreeProgram = "Webdesign and Development program";
+var myKids          = "2 kids";
+var myWork          = "Amazon.com";
+
+//outputs
+
+console.log("Hello my name is " + myFirstName  + myLastName)
+console.log("I am currently enrolled in " +myDegreeProgram)
+console.log("For the last year i have worked for " +myWork)
+console.log("Finally i have " + myKids)
