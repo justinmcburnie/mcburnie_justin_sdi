03ce008b2f35ea548edbfca3c9d79258ae28dea3
justinmcburnie <jmcburnie@fullsail.edu>
2015-10-03 22:54:56 -0400

    Slice of pizza 1
    Finished Slice of pizza set 1

--- Projects/Expressions_worksheet/Exp.js
+++ Projects/Expressions_worksheet/Exp.js
@@ -9,8 +9,15 @@ var actualAge =1; //actual age of dog
 var dogYears  =7; //dog years
 var results   =actualAge * dogYears; //Actual age times years in dog years
 
-console.log(results +" " +"years old")
+console.log("Sparky is" + " "+ actualAge +" "+"human years old" +" "+ "which is"+" "+results +" "+ "in dog years");
 
 
-//
+//Slices of Pizza
+
+var slicePerPizza =8;
+var people        =10;
+var numPizzas     =5;
+var results       =(slicePerPizza * numPizzas / people);
+
+console.log("Each person ate"+" "+ results +" "+ "slices of pizza at the party.");
 
