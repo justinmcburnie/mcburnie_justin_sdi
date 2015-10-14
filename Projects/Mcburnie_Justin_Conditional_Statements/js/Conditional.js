7a130770b05b605a39fbeaf277c2bb68873178c1
justinmcburnie <jmcburnie@fullsail.edu>
2015-10-14 15:16:47 -0400

    conditionals
    if and else if added

--- Projects/Mcburnie_Justin_Conditional_Statements/js/Conditional.js
+++ Projects/Mcburnie_Justin_Conditional_Statements/js/Conditional.js
@@ -3,10 +3,18 @@
 Conditional Statements*/
 
 
-
+var budget          =20000
 var carPrice        =prompt("Please enter car sell price.:");//cars price before taxes and interest
 var tax             =prompt("please enter tax rate: ");//tax on vehicle sell
-/*var interestRate    =prompt("Please enter interest rate(in decimal)");//Interest rate for the vehicle*/
-taxPrice            =(carPrice * tax + carPrice);
-carInterest         =(taxPrice);
-console.log(taxPrice);
+var interestRate    =prompt("Please enter interest rate(in decimal)");//Interest rate for the vehicle
+taxPrice            =Number(carPrice * tax);
+carTax              =Number(+carPrice + taxPrice);
+carinterest         =Number(+carTax * interestRate);
+carTotal            =Number(+ carinterest + carTax);
+
+
+if(carTotal <= budget){
+    console.log("We Can buy the car!");
+}else if(carTotal > budget){
+    console.log("Look for a cheaper car.");
+}
