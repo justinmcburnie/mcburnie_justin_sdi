f6e89cf2359341dbccefaadb5266e8331405c481
justinmcburnie <jmcburnie@fullsail.edu>
2015-10-14 05:19:24 -0400

    group3
    started creating my if statements for group 3

--- Conditional_worksheet/JS/week3.js
+++ Conditional_worksheet/JS/week3.js
@@ -1,12 +1,33 @@
 /*justin mcburnie
 week3 worksheet*/
 
-var mpg = prompt("Please enter vehicles MPG:"); //mpg of the vehicle
+/*var mpg = prompt("Please enter vehicles MPG:"); //mpg of the vehicle
 var gasGauge =prompt("Please enter gasGauge in percent. Write as decimal.:");//current gas gauge with percent writeen as deciaml
-var tankCapicity =prompt("Please enter gas tank capacity:");
+var tankCapicity =prompt("Please enter gas tank capacity:");//fuel tank capacity
 totalMiles =    (mpg * tankCapicity);//total miles on full tank
-milesLeft =     (totalMiles * gasGauge);// total miles left
-console.log(milesLeft);
-    if(milesLeft <= 200){
-        console.log("stop!!!!.");
+milesLeft =    Number(totalMiles * gasGauge - 20);// total miles left - 20 miles just to be safe
+
+
+
+    if(milesLeft <= 200) {
+        console.log("You only have" +" "+ milesLeft +" "+ "gallons of gas in your tank, better get gas now while you can!");
+    }else{
+        console.log("Yes, you can make it without stopping for gas!");
     }
+*/
+
+var timeMovie = prompt("Enter Time of Movie.:");
+var age       = prompt("Enter Age:.");
+
+if (age >= 55 || age <= 10) {
+
+   console.log("7");
+}else{
+    console.log("12");
+}
+
+if (timeMovie = 3 && timeMovie <= 5) {
+    console.log("Ticket Price is $7.");
+}else{
+    console.log("12");
+}
